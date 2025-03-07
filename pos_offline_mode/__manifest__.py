b3f61be0d65f9dac8ae88347f35839f95114c778
dhiaabboud <abouddhia9@gmail.com>
2025-03-07 02:14:40 +0100

    first commit

---
+++ pos_offline_mode/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*
+# This Source Codes created by TL Technology (thanhchatvn@gmail.com)
+# Not allow resale, editing source codes
+# License: Odoo Proprietary License v1.0
+{
+    "name": "POS Offline mode",
+    "version": "1.0.3",
+    "category": "Point of Sale",
+    "author": "Odexa",
+    "summary":
+        """
+        Point Of Sale Offline Mode\n
+        Allow Sale Products, create Orders without Internet and Odoo Server \n
+        Allow Reload, Refresh POS Page  without Internet and Odoo Server\n
+        Allow Resume POS Session without Internet and Odoo Server
+        """,
+    "description":
+        """
+        Point Of Sale Offline Mode\n
+        Allow Sale Products, create Order without Internet and Odoo Server \n
+        Allow Reload, Refresh POS Page  without Internet and Odoo Server\n
+        Allow Resume POS Session without Internet and Odoo Server
+        """,
+    "live_test_url": "https://www.youtube.com/watch?v=c36bytTuZXw",
+    "price": "80",
+    "sequence": 0,
+    "depends": [
+        "point_of_sale",
+        "pos_hr",
+        "pos_restaurant",
+    ],
+    "demo": [],
+    "data": [
+        "import_libraries.xml"
+    ],
+    "currency": "EUR",
+    "installable": True,
+    "application": True,
+    "images": ["static/description/icon.png"],
+    "license": "OPL-1",
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_retail_offline/static/src/apps/idb-keyval.js",
+            "pos_retail_offline/static/src/apps/PosIDB.js",
+            "pos_retail_offline/static/src/apps/models.js",
+            "pos_retail_offline/static/src/apps/webClient.js",
+        ],
+        'web.assets_backend': [
+
+        ],
+    },
+}
